0329e60f997ef2fb4711cf4cebe9491096a18e11
Nikhil Lingutla <nlingutla@vmware.com>
2016-08-21 14:59:52 -0700

    Add: Added insertion sort

---
+++ python/sorting/insertion_sort/insertion_sort.py
@@ -0,0 +1,48 @@
+import unittest
+
+def insertion_sort(nums):
+	length = len(nums)
+	for i in range(1, length):
+		j = i
+		while nums[j] < nums[j-1] and j>0:
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+			j = j-1
+
+	return nums
+
+
+class TestInsertionSort(unittest.TestCase):
+
+	def test_insertio_sort_1(self):
+		given = [2,4,3,1,5]
+		expected = [1,2,3,4,5]
+		self.assertEqual(expected, insertion_sort(given))
+
+	def test_insertio_sort_2(self):
+		given = [5,4,3,2,1]
+		expected = [1,2,3,4,5]
+		self.assertEqual(expected, insertion_sort(given))
+
+	def test_insertio_sort_3(self):
+		given = [2,7,4,1,5,3]
+		expected = [1,2,3,4,5,7]
+		self.assertEqual(expected, insertion_sort(given))
+
+	def test_insertio_sort_4(self):
+		given = [2]
+		expected = [2]
+		self.assertEqual(expected, insertion_sort(given))
+
+	def test_insertio_sort_5(self):
+		given = []
+		expected = []
+		self.assertEqual(expected, insertion_sort(given))
+
+	def test_insertio_sort_6(self):
+		given = [1,3,5,7,9,10]
+		expected = [1,3,5,7,9,10]
+		self.assertEqual(expected, insertion_sort(given))
+
+
+if __name__ == "__main__":
+	unittest.main()
